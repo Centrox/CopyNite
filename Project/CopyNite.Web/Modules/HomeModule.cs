b0f88746379e9e894b4eef7de6f07c6a08448f03
Denzj <D.Jacobs@diract-it.nl>
2012-11-18 16:17:52 +0100

    Initial creation

---
+++ Project/CopyNite.Web/Modules/HomeModule.cs
@@ -0,0 +1,19 @@
+﻿using Nancy;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace CopyNite.Web
+{
+    public class HomeModule : NancyModule
+    {
+        public HomeModule()
+        {
+            Get["/"] = parameters =>
+            {
+                return View["Index"];
+            };            
+        }
+    }
+}
